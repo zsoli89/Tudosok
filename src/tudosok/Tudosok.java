891dcab27f271403ea5f5cafdfade3d8e383248f
zsoli89 <jakabzsoli89@gmail.com>
2022-05-15 15:27:44 +0200

    Added TudosokFXML.fxml, TudosokFXController.java Changed Tudosok.java

--- src/tudosok/Tudosok.java
+++ src/tudosok/Tudosok.java
@@ -1,9 +1,28 @@
 package tudosok;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author jakab
  */
-public class Tudosok {
-    
+public class Tudosok extends Application{
+   @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TudosokFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Hello");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }     
 }
